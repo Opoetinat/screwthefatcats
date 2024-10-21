fd5ba552a0cf9415272bbd5ccc7ef3ee90ca3e7c
Opoetinat <lisageorgejimenez@gmail.com>
2024-10-21 14:08:12 -0400

    init

---
+++ js/about.01a2161c.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkMovieLair"]=self["webpackChunkMovieLair"]||[]).push([[594],{5609:function(n,u,a){a.r(u),a.d(u,{default:function(){return o}});var e=a(6768);const r={class:"about"};function t(n,u){return(0,e.uX)(),(0,e.CE)("div",r,u[0]||(u[0]=[(0,e.Lk)("h1",null,"This is an about page",-1)]))}var i=a(1241);const s={},c=(0,i.A)(s,[["render",t]]);var o=c}}]);
+//# sourceMappingURL=about.01a2161c.js.map
